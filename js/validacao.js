45d1d9db02d0b965e6d9d1b245b64c399cabaa87
Jessica Ferraz <ferrazjaa@gmail.com>
2024-07-02 18:45:01 -0300

    update #25
    mensagem de agradecimento a quem entrar em contato

---
+++ js/validacao.js
@@ -0,0 +1,17 @@
+function validacao(event) {
+    event.preventDefault();
+
+    var nome = document.getElementById('nome-reserve').value;
+    var email = document.getElementById('e-mail').value;
+    var message = document.getElementById('mensagem').value;
+    if (!nome || !email || !message) {
+        alert("Por favor, preencha os campos corretamente");
+        return;
+    }
+
+    document.getElementById('resposta').innerText = `${nome}, obrigada por entrar em contato! <br> Assim que possível retornaremos.`;
+
+    return false;
+
+
+}
